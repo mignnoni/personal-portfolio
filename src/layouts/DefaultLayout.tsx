8a110535231b7147caf25dd3377d4a80fde2ca1e
Iuri Mignoni <iurimig@gmail.com>
2023-08-30 13:02:51 -0300

    home page

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -5,9 +5,9 @@ import { Outlet } from "react-router-dom";
 export function DefaultLayout() {
     return (
         <Flex minH={'100vh'} minW={'100vw'}>
-            <Flex w="100%" my={12} minW={1200} mx="auto" maxW={1200} align={'center'} flexDir={'column'}>
+            <Flex w="100%" my={12} minW={1000} mx="auto" maxW={1000} align={'center'} flexDir={'column'}>
                 <Header />
-                {/* <Outlet /> */}
+                <Outlet />
             </Flex>
         </Flex>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Heading, Stack } from "@chakra-ui/react";
 
 export function Home() {
     return (
-        <Box w={"full"} bgColor={'gray.100'}>
-            Hello World
-        </Box>
+        <Flex w={"full"} justify={'space-between'} mt={12}>
+            <Stack>
+                <Heading as='h1'>Iuri Mignoni</Heading>
+                <Heading as='h2' color={'purple.300'}>Desenvolvedor Web Full Stack</Heading>
+            </Stack>
+            <Flex>
+                A2
+            </Flex>
+        </Flex>
     );
 }
